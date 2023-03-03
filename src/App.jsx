262134e4aa87d74e7f9d7f47c048ffc944c56f9b
Moussaab Badla <moussaab.badla@etu.usthb.dz>
2023-03-03 12:37:01 +0100

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import  Heading  from "./component/Heading"
+function App() {
+  return (
+    
+  <div className=" text-red-400">
+        <Heading />
+      
+      
+    </div>
+  )
+}
+
+
+export default App
---
+++ src/component/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List(){
+    return (
+        <div>
+            <ul>
+                <li>A</li>
+                <li>B</li>
+                <li>C</li>
+            </ul>
+        </div>
+    )
+}
